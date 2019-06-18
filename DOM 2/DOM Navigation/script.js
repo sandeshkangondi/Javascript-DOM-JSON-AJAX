b8d0da542e7ce5b41be9c4fd73f44b5d83713d8a
sandeshkangondi <51419528+sandeshkangondi@users.noreply.github.com>
2019-06-17 18:30:35 -0700

    DOM_2

---
+++ DOM 2/DOM Navigation/script.js
@@ -0,0 +1,35 @@
+/*
+var listItem = document.getElementById('list-item');
+
+console.log(listItem.parentNode);
+console.log(listItem.parentNode.parentNode);
+console.log(listItem.parentElement);
+console.log(listItem.parentElement.style.background = 'orange');
+console.log(listItem.childNodes);
+console.log(listItem.children);
+console.log(listItem.firstChild);
+console.log(listItem.firstElementChild);
+console.log(listItem.lastChild);
+console.log(listItem.lastElementChild);
+console.log(listItem.previousSibling);
+console.log(listItem.previousElementSibling);
+console.log(listItem.nextElementSibling);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
